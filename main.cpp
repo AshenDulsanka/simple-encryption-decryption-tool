409404f65f21d68783af9ba664190ebd3a59d7f0
Ashen Abeysekara <ashendul@gmail.com>
2024-07-05 11:15:54 +0530

    added new files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "encryption.h"
+#include <string>
+
+using namespace std;
+
+int main(){
+    string filename;
+    char mode;
+
+    cout << "Enter the name of the file: ";
+    return 0;
+}
