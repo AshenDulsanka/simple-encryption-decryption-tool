cb63758ab1897df66919ca3532c3d8c851a46419
Ashen Abeysekara <ashendul@gmail.com>
2024-09-10 10:13:17 +0530

    finished headers

--- main.cpp
+++ main.cpp
@@ -9,5 +9,14 @@ int main(){
     char mode;
 
     cout << "Enter the name of the file: ";
+    getline(cin >> std::ws, filename); //read all and ignores all the white spaces from the filename string
+
+    cout << "Encrypt or Decrypt? (e/d): ";
+    cin >> mode;
+
+    if(mode == 'e' || mode == 'E'){
+        
+    }
+
     return 0;
 }
