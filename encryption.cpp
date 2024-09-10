d59d72cfbb2370bf816f47c8c39475f19c467759
Ashen Abeysekara <ashendul@gmail.com>
2024-09-10 20:00:27 +0530

    encryptFile function updated

--- encryption.cpp
+++ encryption.cpp
@@ -5,5 +5,17 @@
 using namespace std;
 
 bool encryptFile(const string& filename, bool encrypt){
-    
+    ifstream inFile(filename); // Open file for reading
+    if (!inFile){
+        return false;
+    }
+
+    // read the file content
+    string content((istreambuf_iterator<char>(inFile)), {});
+    inFile.close();
+
+    // encrypt the content
+    if (performCeaserCipher(content, encrypt)){
+        
+    }
 }
